9872863942c0b5bd8e9502d965fa37bbf8caa801
eejonghwa <teriyaki970326@gmail.com>
2023-09-11 10:59:47 +0900

    게시판 게시물 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,6 @@ public class Main {
 //        System.out.println(str.equals("hello"));
 
 
-
         // 이름 짓는 규칙
         // 변수, 클래스 -> 명사
         // 함수 -> 동사
@@ -32,17 +31,25 @@ public class Main {
 
         while (true) {
 
-            System.out.println("메뉴 입력 : ");
+            System.out.print("명령어 : ");
 
             String command = scan.nextLine();
-            if(command.equals("exit")){
+            if (command.equals("exit")) {
+
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            } else if (command.equals("add")) {
+                System.out.print("게시물 제목을 입력해주세요.");
+                String title = scan.nextLine();
+                System.out.print("게시물 내용을 입력해주세요.");
+                String content = scan.nextLine();
+
+                System.out.println("게시물이 등록되었습니다.");
+
+
             }
-
         }
-
-
     }
 }
+
 
